bab17be48b49cedf23bfeb1953064ecbb8bb4923
Wade Wallace <77767227+wadallace@users.noreply.github.com>
2024-06-19 16:26:25 +0000

    added footnotes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,6 +16,37 @@ const Footer = () => {
         >
           <p>© 2024 Wade</p>
         </Flex>
+        <Flex margin='0 auto'
+          fontSize={10}
+          color='white'
+          justifyContent='center'
+          alignItems='center'
+          maxWidth='1024px'>
+          <p>
+            * Not really, my name is Wade, and the Avatar image is supplied by Meta for the project
+          </p>
+        </Flex>
+        <Flex margin='0 auto'
+          fontSize={10}
+          color='white'
+          justifyContent='center'
+          alignItems='center'
+          maxWidth='1024px'>
+          <p>
+            ** Sort of
+          </p>
+        </Flex>
+        <Flex margin='0 auto'
+          fontSize={10}
+          color='white'
+          justifyContent='center'
+          alignItems='center'
+          maxWidth='1024px'
+          paddingBottom={4}>
+          <p>
+            *** Aspirationally
+          </p>
+        </Flex>
       </footer>
     </Box>
   )
--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -2,9 +2,9 @@ import React from "react"
 import { Avatar, Heading, VStack } from "@chakra-ui/react"
 import FullScreenSection from "./FullScreenSection"
 
-const greeting = "Hello, I am Pete!"
-const bio1 = "A frontend developer"
-const bio2 = "specialised in React"
+const greeting = "Hello, I am Pete!*"
+const bio1 = "A frontend developer**"
+const bio2 = "specialised in React***"
 
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
